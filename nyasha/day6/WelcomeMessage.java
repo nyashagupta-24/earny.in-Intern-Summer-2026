5b84bb60dd4510d99a7efd3e38ead9f3ecc33ad7
nyashagupta-24 <nyashagupta.rprn@gmail.com>
2026-06-21 14:54:47 +0530

    assignment6

---
+++ nyasha/day6/WelcomeMessage.java
@@ -0,0 +1,13 @@
+public class WelcomeMessage {
+
+    public static void welcome_message() {
+        System.out.println("================================");
+        System.out.println("Welcome to the Internship Program");
+        System.out.println("Learn • Build • Grow");
+        System.out.println("================================");
+    }
+
+    public static void main(String[] args) {
+        welcome_message();
+    }
+}
