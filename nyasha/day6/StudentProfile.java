fb23193d900b79ab68af430fbbba03d60359ebb0
nyashagupta-24 <nyashagupta.rprn@gmail.com>
2026-06-21 15:06:56 +0530

    assignment6

---
+++ nyasha/day6/StudentProfile.java
@@ -0,0 +1,15 @@
+public class StudentProfile {
+
+    public static void student_profile() {
+        System.out.println("Student Profile");
+        System.out.println();
+        System.out.println("Name: Sneha");
+        System.out.println("Roll Number: CS101");
+        System.out.println("Branch: Computer Science");
+        System.out.println("Year: First Year");
+    }
+
+    public static void main(String[] args) {
+        student_profile();
+    }
+}
